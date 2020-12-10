9414319cc603a98a53b84c0c69c130d04ef489b4
Steele Price <stprice@courts.az.gov>
2020-12-09 19:04:07 -0700

    clean-up scripts

--- .devcontainer/scripts/setup-dotnet.sh
+++ .devcontainer/scripts/setup-dotnet.sh
@@ -3,11 +3,7 @@
 # we use APT over curl, if you want nightlies, make a branch.
 # we need 3.1 for notebooks for now, you can opt multiple versions
 
-PACKAGE_LIST = "dotnet-sdk-3.1 powershell "
-
-if [ "${INSTALL_DOTNET5}" = "true" ]; then
-
-    PACKAGE_LIST = ${PACKAGE_LIST} + "dotnet-sdk-5.0 " 
+INSTALL_DOTNET_PKGS = ${1:-"dotnet-sdk-3.1 powershell"}
 
     echo "Packages to verify are installed: ${PACKAGE_LIST}"
     apt-get-update-if-needed
--- .devcontainer/scripts/setup-user.sh
+++ .devcontainer/scripts/setup-user.sh
@@ -1,56 +1,35 @@
 # Install User - create dotfiles and prompts
- # Do not run this as root or it won't work as expected 
 
-# [Option] Install zsh
-ARG INSTALL_ZSH="true"
-# User run command path, tilde ~ won't work for this.
-USER_RC_PATH="/home/${USERNAME}"
+USERNAME = ${1:-"vscode"}
+USER_UID = ${2:-"1000"}
+USER_GID = ${3:-"1000"}
 
-RC_SNIPPET="$(cat << EOF
-export USER=vscode
-
-export PATH=\$PATH:\$HOME/.local/bin
-EOF
-)"
+if [ "$(id -u)" -eq 0 ]; then
+    echo -e 'Script cannot be run as root. We are setting up a non-priviledged user.'
+    exit 1
+else
+    make-user
+    install-omz
+    apt-get-update-if-needed
+    apt-get install -y zsh
+    dotnet tool restore
+    clone-powerline-fonts
+    mv /tmp/dotfiles/ /home/${USERNAME}
+    chown dotfiles
+    cd /home/${USERNAME}/dotfiles
+    make all
+    cd ~
+    echo "setup-user.sh done."
+    exit 0
+fi
 
 # Create or update a non-root user to match UID/GID.
-make_user() {
-    useradd -s /bin/bash --uid $USER_UID --gid $USER_GID -m $USERNAME
-    groupadd --gid $USER_GID $USERNAME
-    usermod --gid $USER_GID $USERNAME
-    usermod --uid $USER_UID $USERNAME
-}
-
-# Add add sudo support for non-root user
-if [ "${USERNAME}" != "root" ] && [ "${EXISTING_NON_ROOT_USER}" != "${USERNAME}" ]; then
+make-user() {
+    sudo useradd -s /bin/bash --uid $USER_UID --gid $USER_GID -m $USERNAME
+    # Add add sudo support for non-root user
     echo $USERNAME ALL=\(root\) NOPASSWD:ALL > /etc/sudoers.d/$USERNAME
-    chmod 0440 /etc/sudoers.d/$USERNAME
-    EXISTING_NON_ROOT_USER="${USERNAME}"
-fi
-
-
-# code shim, it fallbacks to code-insiders if code is not available
-cat << 'EOF' > /usr/local/bin/code
-#!/bin/sh
-
-get_in_path_except_current() {
-  which -a "$1" | grep -v "$0" | head -1
+    sudo chmod 0440 /etc/sudoers.d/$USERNAME
 }
-
-code="$(get_in_path_except_current code)"
-
-if [ -n "$code" ]; then
-  exec "$code" "$@"
-elif [ "$(command -v code-insiders)" ]; then
-  exec code-insiders "$@"
-else
-  echo "code or code-insiders is not installed" >&2
-  exit 127
-fi
-EOF
-chmod +x /usr/local/bin/code
-}
-
 
 install-omz()
 {
@@ -58,49 +37,16 @@ install-omz()
     if $(ls $(zshDir)) != ""
         ZSH=${ZSH:-${zshDir}}
         sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh) --unattended"
-        sed -i -e 's/ZSH_THEME=.*/ZSH_THEME="'${OH_MY_THEME}'"/g' ${USER_RC_FILE}
     else
         echo "oh-my-zsh already there."
     fi
 }
 
-if [ "${RC_SNIPPET_ALREADY_ADDED}" != "true" ]; then
-    echo "${RC_SNIPPET}" >> /etc/bash.bashrc
-    RC_SNIPPET_ALREADY_ADDED="true"
-fi
-
-# Install and configure zsh and Oh My Zsh!
-apt-get-update-if-needed
-apt-get install -y zsh
-if [ "${ZSH_ALREADY_INSTALLED}" != "true" ]; then
-    echo "${RC_SNIPPET}" >> /etc/zsh/zshrc
-    ZSH_ALREADY_INSTALLED="true"
-fi
-
-install-omz
-
-    # install the dotnet tools we know we want
-    # see dotnet-tools.json
-    # && dotnet tool restore
-    #
-    # these go here:
-    # Global tools are installed in the following directories by default when you specify the -g or --global option:
-    # Linux/macOS:  $HOME/.dotnet/tools
-    
-    # add scripting
-    dotnet tool install -g dotnet-script
-    # add API tooling
-    dotnet tool install -g APIPort
-    # add conversion utility for Dotnet Framework conversion
-    /bin/su -c sh dotnet tool install -g try-convert - ${USERNAME}
-    # add Project Tye
-    /bin/su -c sh dotnet tool install -g Microsoft.Tye --version "0.6.0-*" --add-source https://pkgs.dev.azure.com/dnceng/public/_packaging/dotnet5/nuget/v3/index.json - ${USERNAME}
-
-fonts:
-    # # clone patched powerline fonts
+clone-powerline-fonts() 
+{
     git clone https://github.com/powerline/fonts.git --depth=1
     cd fonts
     ./install.sh
     cd ..
     rm -rf fonts
-fi
+}
